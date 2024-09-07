d13bc0f25d70c37d5a1774d6442c9d9597c21621
14himanshi <102136102+14himanshi@users.noreply.github.com>
2024-09-07 05:01:08 +0000

    StackHeap

---
+++ 01_Basics/StackHeapMemory.js
@@ -0,0 +1,22 @@
+// Stack (Primitive) and Heap (Non-Primitive)
+
+let myMain = "himanshidotcom"
+let anotherName = myMain
+anotherName = "himanshiVyas"
+
+console.log(myMain) // himanshidotcom
+console.log(anotherName) // himanshiVyas
+
+let userOne = {
+    email: "user@hotmail.com",
+    upi: "user@hbcu",
+}
+
+let userTwo = userOne
+userTwo.email = "himanshi@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
+// ********************************************************************
+
