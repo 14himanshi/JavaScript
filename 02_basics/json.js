4260ee6d72d728852d4eb9d25687486f8fb57f1b
14himanshi <102136102+14himanshi@users.noreply.github.com>
2024-09-08 14:01:54 +0000

    json

---
+++ 02_basics/json.js
@@ -0,0 +1,15 @@
+// destructuring in objects
+
+// {
+//     "name": "leo",
+//     "courseName": "js in hindi",
+//     "price": "free"
+// }
+
+// yeh asse array ke form mai bhi aate hai api
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
