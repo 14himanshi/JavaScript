8473048ece078dc6f364c2f7c8c7f26736a29594
14himanshi <102136102+14himanshi@users.noreply.github.com>
2024-09-11 06:15:08 +0000

    chag

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -141,8 +141,50 @@ cpp is at index 4
 // for each loop
 const coding = ['js', 'ruby', 'java', 'python', 'cpp']
 
+// coding.forEach( function (item) {
+//     console.log(item);
+// } )
 
+// coding.forEach ( (item) => {
+//     console.log(item.length);
+    
+// } )
 
+// function printMe(item) {
+//   console.log(item)
+// }
 
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+// } )
+/*
+js 0 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+ruby 1 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+java 2 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+python 3 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+cpp 4 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+*/
+
+const a = [
+  {
+    languageName: "javascript",
+    languageFileName: "js",
+  },
+  {
+    languageName: "java",
+    languageFileName: "java",
+  },
+  {
+    languageName: "python",
+    languageFileName: "py",
+  }
+]
+
+a.forEach( (item) => {
+    console.log(item.languageName);
+    
+} )
 
 
