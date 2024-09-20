cd5473d7bc0544e3b128f4244b6395de183fc834
14himanshi <102136102+14himanshi@users.noreply.github.com>
2024-09-20 16:22:06 +0000

    pro

---
+++ 09_advanced_one/promises.js
@@ -0,0 +1,19 @@
+/*
+
+Promises: Promise is an object which represents the eventual completion (or failure) of an asynchronous operation and it's resulting value
+Promise ka matlab yeh hai ki, aapne jo usko task diya hai woh woh task ko queue mai laga deta hai (and woh tabhi ke tabhi complete nahi hota) but woh jab uska chance aata hai queue mai tabhi complete hota hai. yeh mota mota yeh hi hai ki, yeh ek promise hai ki yeh task badmai hoga(kab hoga woh nahi pata), ho ga ya bhi nahi woh nahi pata, but yeh queue mai daalta hai woh task ko
+yeh operations konse hai? 
+(accessing file, request to databases, cryptography etc yeh sabmai asynchronous programming hota hai ismai async await lagate hai ya promises use karte hai, yeh sabmai zyada time lagta hai)
+
+promises ke three state hote hai:
+- pending: initial state, neither fulfilled nor rejected.
+- fulfilled: meaning that the operation was completed successfully.
+- rejected: meaning that the operation failed
+
+*/
+
+// promises banate bhi hai but mostly hum promises ko consume hi karte hai
+
+
+
+
