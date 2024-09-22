b8c2a93d40ccb630a54575601f9b0987ac0a5bee
14himanshi <102136102+14himanshi@users.noreply.github.com>
2024-09-22 08:41:10 +0000

    changes

---
+++ 11_prototypes/call.js
@@ -0,0 +1,11 @@
+function setUSername(username){
+    // complex DB calls
+    this.username = username
+
+}
+
+function createUser(username, email, password){
+    this.email = email
+    this.password = password
+    setUsername(username)
+}
